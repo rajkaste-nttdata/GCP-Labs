af802a1a120f5d855d67ba0c6148e452965c5245
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-19 02:13:55 +0530

    Update Raj_Kaste_LAB2_Exercise_8.sql

--- Lab2-BigQuery/Raj_Kaste_LAB2_Exercise_8.sql
+++ Lab2-BigQuery/Raj_Kaste_LAB2_Exercise_8.sql
@@ -4,7 +4,7 @@ FROM `bigquery-public-data.github_repos.languages`as l
 JOIN `bigquery-public-data.github_repos.sample_commits` as sc
 on l.repo_name = sc.repo_name,
 UNNEST(language) as lg
-where lg.name = "Java" and 	EXTRACT(YEAR FROM sc.committer.date)=2016
+where lg.name = "Java" and EXTRACT(YEAR FROM sc.committer.date)=2016
 group by name
 order by count desc 
 limit 10

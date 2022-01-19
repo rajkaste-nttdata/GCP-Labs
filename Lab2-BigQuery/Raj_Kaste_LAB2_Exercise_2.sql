e21a6951d5d8744d36e25db2dac1eb3b5d6838b4
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-19 16:14:31 +0530

    Delete Lab2-BigQuery directory

--- Lab2-BigQuery/Raj_Kaste_LAB2_Exercise_2.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-visitid,visitStartTime,h.page.pageTitle as pageTitle ,h.page.pagePath as pagePath
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`,
-UNNEST(hits) AS h;
